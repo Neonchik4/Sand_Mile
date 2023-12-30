4d5c95e2335b8234863e1effce2fad7d8df79aa4
Ivan <vanyagame12@gmail.com>
2023-12-30 22:06:31 +0300

    alpha's rotation was done; map editor 1.0

--- pusk.py
+++ pusk.py
@@ -3,6 +3,37 @@ import os
 import sys
 import random
 import math
+from PIL import Image
+
+
+def image_to_list(file_name):
+    im = Image.open(file_name)
+    pixels = list(im.getdata())
+    width, height = im.size
+    new_pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+    return new_pixels
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            # if level[y][x] == '.':
+            #     Tile('empty', x, y)
+            # elif level[y][x] == '#':
+            #     Tile('wall', x, y)
+            # elif level[y][x] == '@':
+            #     Tile('empty', x, y)
+            #     new_player = Player(x, y)
+            if level[y][x] in tiles_images:
+                Tile(level[y][x], x, y)
+            elif level[y][x] == (136, 0, 21):
+                Tile(base_pixel, x, y)
+                new_player = Player(x, y)
+            else:
+                Tile(base_pixel, x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
 
 def terminate():
@@ -78,27 +109,33 @@ class Camera:
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
-        self.x = pos_x
-        self.y = pos_y
+        self.x = pos_x * tile_width
+        self.y = pos_y * tile_height
         self.image = player_image
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect().move(self.x, self.y)
+        # print(self.x, self.y)
+        # print()
+        # print(self.rect[0], self.rect[1])
+        # в комментариях лежит магический фокус
         self.orig = self.image
 
     def rotate_towards_mouse(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        rel_x, rel_y = mouse_x - (self.x + self.rect[2] // 3), mouse_y - (self.y + self.rect[3] // 3)
-        if ((mouse_x < 456 or mouse_x > 504) or (mouse_y > 501 or mouse_y < 459)) and mouse_y != 391 and mouse_y != 512:
-            angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
-            self.image = pygame.transform.rotate(self.orig, int(angle) - 90)
-            self.rect = self.image.get_rect(center=self.rect.center)
+        rel_x, rel_y = mouse_x - (self.rect[0] + self.rect[2] // 2), mouse_y - (self.rect[1] + self.rect[3] // 2)
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        self.image = pygame.transform.rotate(self.orig, int(angle) - 90)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+        self.image = None
+        if type(tiles_images[tile_type]) == list:  # по неведомой причине через is не работает
+            self.image = random.choice(tiles_images[tile_type])
+        else:
+            self.image = tiles_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 pygame.init()
@@ -116,9 +153,59 @@ camera = Camera()
 STEP = 50
 FPS = 50
 
-player_image = pygame.transform.scale(load_image('units/alpha.png'), (44, 40))
+# Player(WIDTH // 2 - 22, HEIGHT // 2 - 20)
 cursor = pygame.image.load('data/cursor.png')
-player = Player(WIDTH // 2 - 22, HEIGHT // 2 - 20)
+# (136, 0, 21): player
+# цвет игрока обязан присутствовать на поле
+tiles_images = {
+    # ground blocks
+    (0, 0, 0): load_image('tiles/ground/black_tile.png'),
+    (127, 127, 127): [load_image('tiles/ground/basalt_1.png'), load_image('tiles/ground/basalt_2.png'),
+                      load_image('tiles/ground/basalt_3.png')],
+    (255, 128, 255): [load_image('tiles/ground/bluemat1.png'), load_image('tiles/ground/bluemat2.png'),
+                      load_image('tiles/ground/bluemat3.png')],
+    (120, 120, 120): [load_image('tiles/ground/classem-stolnene1.png'),
+                      load_image('tiles/ground/classem-stolnene2.png'),
+                      load_image('tiles/ground/classem-stolnene3.png')],
+    (120, 64, 20): [load_image('tiles/ground/dirt1.png'), load_image('tiles/ground/dirt2.png'),
+                    load_image('tiles/ground/dirt3.png')],
+    (181, 181, 240): [load_image('tiles/ground/ice1.png'), load_image('tiles/ground/ice2.png'),
+                      load_image('tiles/ground/ice3.png')],
+    (251, 82, 0): [load_image('tiles/ground/magmarock1.png'), load_image('tiles/ground/magmarock2.png'),
+                   load_image('tiles/ground/magmarock3.png')],
+    (102, 33, 0): [load_image('tiles/ground/mud1.png'), load_image('tiles/ground/mud2.png'),
+                   load_image('tiles/ground/mud3.png')],
+    (210, 174, 141): [load_image('tiles/ground/sand_ground_1.png'), load_image('tiles/ground/sand_ground_2.png'),
+                      load_image('tiles/ground/sand_ground_3.png')],
+    (60, 56, 56): [load_image('tiles/ground/darksand1.png'), load_image('tiles/ground/darksand2.png'),
+                   load_image('tiles/ground/darksand3.png')],
+    # wall blocks
+    (196, 100, 64): [load_image('tiles/wall_blocks/dirt-wall1.png'), load_image('tiles/wall_blocks/dirt-wall2.png')],
+    (141, 141, 141): [load_image('tiles/wall_blocks/dune-wall1.png'), load_image('tiles/wall_blocks/dune-wall2.png')],
+    (120, 101, 92): [load_image('tiles/wall_blocks/ferric-stone-wall1.png'),
+                     load_image('tiles/wall_blocks/ferric-stone-wall2.png')],
+    (130, 125, 233): [load_image('tiles/wall_blocks/ice-wall1.png'), load_image('tiles/wall_blocks/ice-wall2.png')],
+    (126, 38, 66): [load_image('tiles/wall_blocks/red-stone-wall1.png'),
+                    load_image('tiles/wall_blocks/red-stone-wall2.png'),
+                    load_image('tiles/wall_blocks/red-stone-wall3.png')],
+    (218, 181, 96): [load_image('tiles/wall_blocks/regolith-wall1.png'),
+                     load_image('tiles/wall_blocks/regolith-wall2.png')],
+    (69, 32, 32): [load_image('tiles/wall_blocks/rhyolite-wall1.png'),
+                   load_image('tiles/wall_blocks/rhyolite-wall2.png')],
+    (174, 180, 196): [load_image('tiles/wall_blocks/salt-wall1.png'), load_image('tiles/wall_blocks/salt-wall2.png')],
+    (225, 228, 201): [load_image('tiles/wall_blocks/sand_tile_1.png'), load_image('tiles/wall_blocks/sand_tile_2.png')],
+    (92, 86, 122): [load_image('tiles/wall_blocks/shale-wall1.png'), load_image('tiles/wall_blocks/shale-wall2.png')],
+    (225, 233, 240): [load_image('tiles/wall_blocks/snow-wall1.png'), load_image('tiles/wall_blocks/snow-wall2.png')],
+    (153, 94, 154): [load_image('tiles/wall_blocks/spore-wall1.png'), load_image('tiles/wall_blocks/spore-wall2.png')],
+    (82, 82, 92): [load_image('tiles/wall_blocks/stone-wall1.png'), load_image('tiles/wall_blocks/stone-wall2.png')],
+    (146, 94, 70): [load_image('tiles/wall_blocks/yellow-stone-wall1.png'),
+                    load_image('tiles/wall_blocks/yellow-stone-wall2.png')],
+}
+base_pixel = image_to_list('data/maps/map_tmp.png')[0][0]  # если не знаем какой это пиксель, берём этот
+
+player_image = pygame.transform.scale(load_image('units/alpha.png'), (44, 40))
+player, level_x, level_y = generate_level(image_to_list('data/maps/map_1.png'))
+
 start_screen()
 
 while True:
@@ -152,7 +239,5 @@ while True:
         pygame.mouse.set_visible(False)
         screen.blit(cursor, (x, y))
 
-    tiles_group.draw(screen)
-    player_group.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
